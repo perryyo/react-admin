e924d7c939fd3a98a23fc56e369f922eca8ec33d
ahn <93immm@naver.com>
2018-08-27 11:36:53 +0900

    feat: publishing login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import '../public/Login/Login.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import loginLogo from '../public/Login/loginLogo.png';
 
 class Login extends Component {
@@ -11,6 +13,33 @@ class Login extends Component {
           <div className="login-logo-container">
             <img className="login-logo" src={loginLogo} alt="login-logo" />
           </div>
+
+          <div className="login-id-container">
+            <TextField
+              id="login-input-id"
+              label="Username"
+              type="text"
+              margin="normal"
+            />
+          </div>
+          <div className="login-password-container">
+            <TextField
+              id="login-input-password"
+              label="Password"
+              type="password"
+              margin="normal"
+            />
+          </div>
+
+          <div className="login-submit-container">
+            <Button
+              variant="contained"
+              color="primary"
+            >
+            Login
+            </Button>
+          </div>
+
         </div>
       </div>
     );
